29bb0963ffcaa63ed96b36e299010c866f1bf174
choi-ys <rcn115@naver.com>
2021-01-04 14:45:23 +0900

    [2020-01-04]용석:Initial commit

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// vue.config.js
+module.exports = {
+    // production환경일 때만 작동
+    // lintOnSave: process.env.NODE_ENV !== 'production'
+    
+    // eslint on : true , off : false
+    lintOnSave: true
+  }
