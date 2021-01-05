c2e6e295ed4cff8a57b00b627e5488b2efaf9016
choi-ys <rcn115@naver.com>
2021-01-05 15:21:19 +0900

    [2020-01-05]용석:store 기능 별 함수 분리

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList } from '../api/index.js'
-import { fetchAskList } from '../api/index.js'
-import { fetchJobsList } from '../api/index.js'
+import mutations from "./mutations.js";
+import actions from "./actions.js";
 
 Vue.use(Vuex);
 
@@ -23,39 +22,6 @@ export const store = new Vuex.Store({
       return state.jobList;
     },
   },
-  mutations: {
-    SET_NEWS(state, data){
-      state.newsList = data;
-    },
-    SET_ASK(state, data){
-      state.askList = data;
-    },
-    SET_JOBS(state, data){
-      state.jobList = data;
-    },
-  },
-  actions: {
-    FECTH_NEWS(context){
-      fetchNewsList()
-      .then(response => {
-        console.log(response.data)
-        context.commit('SET_NEWS', response.data);
-      })
-      .catch(error => console.log(error))
-    },
-    FETCH_ASK(context){
-      fetchAskList()
-      .then(response => {
-        console.log(response.data);
-        context.commit('SET_ASK', response.data);
-      })
-    },
-    FETCH_JOBS(context){
-      fetchJobsList()
-        .then(response =>{
-          console.log(response.data);
-          context.commit('SET_JOBS', response.data);
-        })
-    }
-  }
+  mutations: mutations,
+  actions: actions,
 });
