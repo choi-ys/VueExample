4af3f85832b7d1b3b83fc76464b664143fe5d769
choi-ys <rcn115@naver.com>
2021-01-05 14:48:58 +0900

    [2020-01-05]용석:vuex의 mapState를 이용한 store.state 접근

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-      <div v-for="news in this.$store.state.newsList"> {{ news.title }}</div>
+      <div v-for="news in newsList"> {{ news.title }}</div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
+  computed:{
+    ...mapState({
+      newsList: state => state.newsList
+    })
+  },
   created(){
     this.$store.dispatch('FECTH_NEWS')
   }
