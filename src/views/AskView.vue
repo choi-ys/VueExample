8695e3875f6eb31825f3b0925cad72ff09386a1f
choi-ys <rcn115@naver.com>
2021-01-06 11:19:35 +0900

    [2020-01-06]용석:각 화면 List item의 Style 적용

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
-    <p v-for="ask in fetchedAskList"> 
-      <a v-bind:href="`/item/${ask.id}`">{{ ask.title }}</a>
-      <small>{{ ask.time_ago }} {{ ask.user }}</small>
-    </p>
+    <ul class="new-list">
+      <li v-for="item in fetchedAskList" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="new-title">
+            <a v-bind:href="`/item/${item.id}`">{{ item.title }}</a>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -21,6 +35,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
